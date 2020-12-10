e245a3a82126e203c922b4d0d16640c59a3d9f06
CalDev98 <64548197+CalDev98@users.noreply.github.com>
2020-12-10 23:14:14 +0100

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://us-central1-clone-fdbc0.cloudfunctions.net/api", // The API (Cloud function) url
-  // http://localhost:5001/clone-fdbc0/us-central1/api   [Can be use for debugging]
+  baseURL: "", // The API (Cloud function) url
 });
 
 
